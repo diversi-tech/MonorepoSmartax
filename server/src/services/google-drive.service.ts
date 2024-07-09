65791a255b72871264c78f3f0938dd7c2a4f6066
Brachi-Prizent <br6791628@gmail.com>
2024-07-09 15:18:32 +0300

    the project running

--- server/src/services/google-drive.service.ts
+++ server/src/services/google-drive.service.ts
@@ -33,7 +33,7 @@ export class GoogleDriveService{
     try {
       const client=await this.clientService.searchClient(clientId);
       // const folderId = await this.getOrCreateFolder(client.name);
-      const folderId = await this.getOrCreateFolder(client.name);
+      const folderId = await this.getOrCreateFolder(client[0].name);
       const response = await this.drive.files.create({
         requestBody: {
           name: Buffer.from(file.originalname, 'latin1').toString('utf8'),
