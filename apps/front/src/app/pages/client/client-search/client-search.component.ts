229a9056d38e64b5f80622971077b0fc24e60fb1
chayaPaperman <ch6752838@gmail.com>
2024-07-11 10:29:36 +0300

    add favorites function

--- apps/front/src/app/pages/client/client-search/client-search.component.ts
+++ apps/front/src/app/pages/client/client-search/client-search.component.ts
@@ -4,50 +4,60 @@ import { ClientService } from '../../../_services/client.service';
 import { FormControl, FormsModule } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 import { PrimeNGConfig, PrimeTemplate } from 'primeng/api';
-import { AutoCompleteModule, AutoCompleteSelectEvent } from 'primeng/autocomplete';
+import {
+  AutoCompleteModule,
+  AutoCompleteSelectEvent,
+} from 'primeng/autocomplete';
 import { CommonModule, NgIf } from '@angular/common';
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { AddClientComponent } from '../add-client/add-client.component';
 import { TableModule } from 'primeng/table';
 import { Button } from 'primeng/button';
+import { User } from '../../../_models/user.module';
+import { UserService } from '../../../_services/user.service';
+import { TokenService } from '../../../_services/token.service';
 
 @Component({
-    // standalone:true,
-    selector: 'app-client-search',
-    templateUrl: './client-search.component.html',
-    styleUrls: ['./client-search.component.scss'],
-    standalone: true,
-    imports: [
-        AutoCompleteModule,
-        FormsModule,
-        PrimeTemplate,
-        NgIf,
-        TableModule,
-        AddClientComponent,
-        RouterOutlet,
-        Button,
-    ],
+  selector: 'app-client-search',
+  templateUrl: './client-search.component.html',
+  styleUrls: ['./client-search.component.scss'],
+  standalone: true,
+  imports: [
+    AutoCompleteModule,
+    FormsModule,
+    PrimeTemplate,
+    NgIf,
+    TableModule,
+    AddClientComponent,
+    RouterOutlet,
+    Button,
+  ],
 })
-export class ClientSearchComponent implements OnInit  {
-  filterNumber: string = "";
+export class ClientSearchComponent implements OnInit {
+  filterNumber: string = '';
   isSelected: number = 0;
   clients: Client[] = [];
   filteredClients: Client[] = [];
   searchName = new FormControl('');
   selectedClient: Client | null = null;
   displayDialog: boolean = false;
+  user: User;
 
   constructor(
     private clientService: ClientService,
+    private userService: UserService,
+    private tokenService: TokenService,
     private router: Router,
     private primengConfig: PrimeNGConfig,
     private route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.primengConfig.ripple = true; // Enable Ripple effect globally
-
-    this.searchName.valueChanges.pipe(debounceTime(300)).subscribe(value => {
+    // this.userService.findOne(this.tokenService.getCurrentDetail("role")._id).subscribe({
+    //   //next(res:User):this.user=res
+    // });
+    this.searchName.valueChanges.pipe(debounceTime(300)).subscribe((value) => {
       if (value !== null) {
         this.filterClientsByNameAndBusinessName(value); // Pass the value directly to filterClients
       }
@@ -56,7 +66,7 @@ export class ClientSearchComponent implements OnInit  {
   }
 
   loadAllClients(): void {
-    this.clientService.getAllClients().subscribe(clients => {
+    this.clientService.getAllClients().subscribe((clients) => {
       this.clients = clients;
       this.filteredClients = clients;
     });
@@ -64,11 +74,15 @@ export class ClientSearchComponent implements OnInit  {
 
   selectClient(event: AutoCompleteSelectEvent): void {
     const client = event.value as Client;
-    this.router.navigate(['/clientSearch/clientManagement'], { state: { client } });
+    this.router.navigate(['/clientSearch/clientManagement'], {
+      state: { client },
+    });
   }
 
   selectClientFromList(client: Client): void {
-    this.router.navigate(['/clientSearch/clientManagement'], { state: { client } });
+    this.router.navigate(['/clientSearch/clientManagement'], {
+      state: { client },
+    });
   }
 
   onSelectionChange(event: Event) {
@@ -77,21 +91,24 @@ export class ClientSearchComponent implements OnInit  {
   }
 
   filterClientsByNameAndBusinessName(value: string): void {
-    if (value !== "") {
+    if (value !== '') {
       const query = value.toLowerCase();
-      this.filteredClients = this.clients.filter(client =>
-        (client.name && client.name.toLowerCase().includes(query)) ||
-        (client.businessName && client.businessName.toLowerCase().includes(query))
+      this.filteredClients = this.clients.filter(
+        (client) =>
+          (client.name && client.name.toLowerCase().includes(query)) ||
+          (client.businessName &&
+            client.businessName.toLowerCase().includes(query))
       );
     }
     this.selectedClient = null;
   }
 
   filterClientsByNumber(): void {
-    if (this.filterNumber != "")
-      this.filteredClients = this.clients.filter(client => client.contactInfo.includes(this.filterNumber));
-    else
-      this.filteredClients = this.clients;
+    if (this.filterNumber != '')
+      this.filteredClients = this.clients.filter((client) =>
+        client.contactInfo.includes(this.filterNumber)
+      );
+    else this.filteredClients = this.clients;
   }
 
   openContactFormDialog() {
@@ -100,5 +117,9 @@ export class ClientSearchComponent implements OnInit  {
 
   closeDialog() {
     this.displayDialog = false;
+  }
+  addFavorite(){
+this.user.favorites
+this.userService.update(this.user._id,this.user.userName, this.user.email,this.user.passwordHash,this.user.role,this.user.favorites,)
   }
 }
