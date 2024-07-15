dd27168ddf5b9ed861b5cee6752b389229e3516e
Tamar055678 <tamar055678@gmail.com>
2024-07-15 10:40:14 +0300

    fix payment model

--- server/src/services/payment.service.ts
+++ server/src/services/payment.service.ts
@@ -10,6 +10,7 @@ import { CreateBillingDto } from "../Models/dto/billing.dto";
 import { PaymentDetails } from "../Models/paymentDetails.model";
 import { CreatePaymentDetailsDto } from "../Models/dto/paymentDetails.dto";
 import { PaymentDetailsService } from "./paymentDetails.service";
+import { ObjectId } from "typeorm";
 
 
 
@@ -17,30 +18,30 @@ import { PaymentDetailsService } from "./paymentDetails.service";
 @Injectable()
 export class PaymentService {
 
-    constructor(@InjectModel('Payment') private readonly PaymentModel: Model<Payment>,private billingService: BillingsService,private PaymentDetailsService: PaymentDetailsService) {}
+    constructor(@InjectModel('Payment') private readonly PaymentModel: Model<Payment>, private billingService: BillingsService, private PaymentDetailsService: PaymentDetailsService) { }
 
     async createPayment(createPaymentDto: CreatePaymentDto): Promise<Payment> {
-        const { paymentDetails,totalPayment,paymentMethod,paymentHistory,billingHistory } = createPaymentDto;
+        const { mainPaymentDetails, morePaymentDetails, totalPayment, paymentMethod, paymentHistory, billingHistory } = createPaymentDto;
 
-        if (!paymentDetails || !paymentMethod) {
-          throw new ValidationException('Missing required fields');
+        if (!mainPaymentDetails || !paymentMethod) {
+            throw new ValidationException('Missing required fields');
         }
-        const createdPayment = new this.PaymentModel({ paymentDetails,totalPayment,paymentMethod,paymentHistory,billingHistory });
+        const createdPayment = new this.PaymentModel({ mainPaymentDetails, morePaymentDetails, totalPayment, paymentMethod, paymentHistory, billingHistory });
         return await createdPayment.save();
     }
 
-    async getAllFrequencies(): Promise<Payment[]> {
+    async getAllPayments(): Promise<Payment[]> {
         return await this.PaymentModel.find().exec();
     }
-    async searchPayment(id:string): Promise<Payment[]> {
-        const Payment= await this.PaymentModel.find({"_id":id}).exec();
+    async searchPayment(id: string): Promise<Payment[]> {
+        const Payment = await this.PaymentModel.find({ "_id": id }).exec();
         if (!Payment || Payment.length === 0) {
             throw new NotFoundException('Payment not found');
-          }
-          return Payment;
+        }
+        return Payment;
     }
     async updatePayment(updatePaymentDto: UpdatePaymentDto): Promise<Payment> {
-        const {id, ...updateData } = updatePaymentDto;
+        const { id, ...updateData } = updatePaymentDto;
         const updatedPayment = await this.PaymentModel.findByIdAndUpdate(id, updateData, { new: true });
         if (!updatedPayment) {
             throw new NotFoundException(`Payment with ID ${id} not found`);
@@ -55,39 +56,69 @@ export class PaymentService {
         }
         return !!deletedPayment;
     }
-    
+
 
     async addBillingToPayment(paymentId: string, billing: CreateBillingDto): Promise<Payment> {
         const payment = await this.PaymentModel.findById(paymentId);
         if (!payment) {
-          throw new NotFoundException('Payment not found');
+            throw new NotFoundException('Payment not found');
         }
-      
-        // הנח ש-createBilling מחזירה אובייקט שמכיל ObjectId מסוג TypeORM
+
         const newBilling = await this.billingService.createBilling(billing);
-      
-        // המרת TypeORM ObjectId ל-Mongoose ObjectId
-        const mongooseObjectId = new Types.ObjectId(newBilling._id.toString());
-      
-        payment.billingHistory.push(mongooseObjectId);
-      
-        return payment.save();
-      }
+
+        try {
+            payment.billingHistory.push(newBilling);
+            return payment.save();
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
 
-      async updatePaymentDetails(paymentId: string, newPaymentDetails: CreatePaymentDetailsDto): Promise<Payment> {
+    async changePaymentDetails(paymentId: string, newPaymentDetails: CreatePaymentDetailsDto): Promise<Payment> {
         const payment = await this.PaymentModel.findById(paymentId);
         if (!payment) {
-          throw new NotFoundException('Payment not found');
+            throw new NotFoundException('Payment not found');
         }
         // Add the current paymentDetails to paymentHistory
-        const currentPaymentDetails = payment.paymentDetails;
+        const currentPaymentDetails = payment.mainPaymentDetails;
         if (currentPaymentDetails && currentPaymentDetails._id) {
-            const mongooseObjectId = new Types.ObjectId(currentPaymentDetails._id.toString());
-          payment.paymentHistory.push(mongooseObjectId);
+            currentPaymentDetails.dateFinish = new Date();
+            payment.paymentHistory.push(currentPaymentDetails);
         }
         // Update the paymentDetails with the new data
-        payment.paymentDetails =await this.PaymentDetailsService.createPaymentDetails(newPaymentDetails);
+        payment.mainPaymentDetails = await this.PaymentDetailsService.createPaymentDetails(newPaymentDetails);
         return payment.save();
-      }
+    }
+
+    async addMorePaymentDetails(paymentId: string, newPaymentDetails: CreatePaymentDetailsDto): Promise<Payment> {
+        const payment = await this.PaymentModel.findById(paymentId);
+        if (!payment) {
+            throw new NotFoundException('Payment not found');
+        }
+        const newMorePaymentDetails = await this.PaymentDetailsService.createPaymentDetails(newPaymentDetails);
+        if (newMorePaymentDetails)
+            payment.morePaymentDetails.push(newMorePaymentDetails);
+        return payment.save();
+    }
+
+    async deleteOldPaymendDetails() {
+        const allPayments = await this.getAllPayments();
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); // לאפס את השעה כדי להתייחס רק לתאריך
+
+        for (const payment of allPayments) {
+            const updatedMorePaymentDetails = payment.morePaymentDetails.filter((detail: PaymentDetails) => {
+                if (detail.dateFinish && new Date(detail.dateFinish) < today) {
+                    payment.paymentHistory.push(detail);
+                    return false; // מסיר מהמערך
+                }
+                return true; // שומר במערך
+            });
+
+            payment.morePaymentDetails = updatedMorePaymentDetails;
+            await payment.save();
+        }
+    
+    }
 }
