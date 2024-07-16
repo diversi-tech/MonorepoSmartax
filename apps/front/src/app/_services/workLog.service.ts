4f26c2ac3c45ed8a2a1be7fd227d995afdd998d1
EttyLehiman <etti378@gmail.com>
2024-07-16 13:02:34 +0300

    zzzzzz

--- apps/front/src/app/_services/workLog.service.ts
+++ apps/front/src/app/_services/workLog.service.ts
@@ -59,6 +59,43 @@
 //     return this.http.put(`${this.apiUrl}/${id}`, { timeEntries });
 //   }
 // }
+//===========================//
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { map, Observable } from 'rxjs';
+// import { WorkLog } from '../_models/workLog.model';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class WorkLogService {
+//   private apiUrl = 'http://localhost:8080/work-log';
+
+//   constructor(private http: HttpClient) {}
+
+//   getWorkLogs(employeeId?: string): Observable<WorkLog[]> {
+//     let url = this.apiUrl;
+//     if (employeeId) {
+//       url += `?employeeId=${employeeId}`;
+//     }
+//     return this.http.get<{ data: WorkLog[] }>(url).pipe(
+//       map(response => response.data)
+//     );
+//   }
+
+//   createWorkLog(workLog: WorkLog): Observable<WorkLog> {
+//     return this.http.post<WorkLog>(this.apiUrl, workLog);
+//   }
+
+//   updateWorkLog(id: string, timeEntries: any[]): Observable<WorkLog> {
+//     return this.http.put<WorkLog>(`${this.apiUrl}/${id}`, { timeEntries });
+//   }
+
+//   exportWorkLogs(month: number, year: number): Observable<Blob> {
+//     const url = `${this.apiUrl}/export/${month}/${year}`;
+//     return this.http.get(url, { responseType: 'blob' });
+//   }
+// }
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
