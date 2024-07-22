1463db2ce4410382d3fa1f7a0357fe8c23000200
Chasi-Ben-Menachem <chasi545988@gmail.com>
2024-07-22 14:58:41 +0300

    style profil client

--- apps/front/src/app/app-routing.module.ts
+++ apps/front/src/app/app-routing.module.ts
@@ -216,6 +216,8 @@ import { EditClientTaxRefunedComponent } from './pages/Manager/edit-client-yearl
       { path: 'monthlyReport', component: MonthlyReportComponent },
       { path: 'taxRefunds', component: TaxRefundsComponent },
       { path: 'financialStatement', component: FinancialStatementComponent },
+      {path:'sensitiveDetails', component:SensitiveDetailsComponent}
+  
     ],
   },
   { path: 'yearlyReport',component: YearlyReportComponent, data:{ authType: 10, forToolbar: false, label: 'Mod', icon: '' }},
