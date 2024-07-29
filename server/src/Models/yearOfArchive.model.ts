18c33bc55db3d82a71ff5d72d3be82e577e9e42c
RacheliRosen <rg0504167401@gmail.com>
2024-07-29 11:31:15 +0300

    year  starting

---
+++ server/src/Models/yearOfArchive.model.ts
@@ -0,0 +1,14 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+
+@Schema()
+export class YearOfArchive extends Document{
+ @Prop()
+ YearNum:string;
+ @Prop()
+ data:[{}];
+}
+export const YearOfArchiveModel = SchemaFactory.createForClass(YearOfArchive)
+
+
