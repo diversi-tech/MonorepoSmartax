603ce26d7e17ddce02c5ddd40f4e16ad90ea2999
Sari-Frenkel <s0548549797@gmail.com>
2024-07-30 13:19:21 +0300

    good

--- apps/front/src/app/pages/client/monthly-report/monthly-report.component.ts
+++ apps/front/src/app/pages/client/monthly-report/monthly-report.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, numberAttribute } from '@angular/core';
 import { Injectable, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -8,9 +8,10 @@ import { TableModule } from 'primeng/table';
 import { MonthlyReportService } from '../../../_services/monthlyReport.service';
 import { MonthlyReport } from '../../../_models/monthlyReport.module';
 import { Client } from '../../../../../../../server/src/Models/client.model';
-import { DatePipe } from '@angular/common';
 import { TreeTableModule } from 'primeng/treetable';
 import { stepFieldMonth } from '../../../_models/stepFieldMonth.module';
+import { YearService } from '../../../_services/year.service';
+import { Year } from '../../../_models/year.module';
 @Component({
   selector: 'app-monthly-report',
   standalone: true,
@@ -29,64 +30,63 @@ import { stepFieldMonth } from '../../../_models/stepFieldMonth.module';
   providedIn: 'root',
 })
 export class MonthlyReportComponent implements OnInit {
-  select: any;
-  constructor(private monthlyReportService: MonthlyReportService) {}
+  constructor(private monthlyReportService: MonthlyReportService,
+    private yearService: YearService
+  ) { }
   ngOnInit(): void {
     this.client = history.state.client;
-    //this.getMonthlyReports()
-   // this.getMonthlyReportsForClientF();
-    this.getMonthlyReports();
+    this.yearService.getAllYear().subscribe({
+      next: (data) => {
+        this.years = data;
+
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    },
+    );
+    this.getMonthlyReportsForClient();
+
     this.getStepByType('מעם');
   }
-
-  dates: any = [{ "num": "01" }, { "num": "02" }, { "num": "03" }, { "num": "04" }, { "num": "05" }, { "num": "06" }, { "num": "07" }, { "num": "08" }, { "num": "09" }, { "num": "10" }, { "num": "11" }, { "num": "12" }];
-  // currentDate = new Date();
-  selectedDate: string;
-  // selectedDate: Date = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() - 1, this.currentDate.getDate());
-  // datePipe: DatePipe = new DatePipe('en-US');
-  //formattedDate: string = this.datePipe.transform(this.selectedDate, 'yyyy-MM-dd');
+  years: Year[] = [];
+  months: any = [{ "num": "01" }, { "num": "02" }, { "num": "03" }, { "num": "04" }, { "num": "05" }, { "num": "06" }, { "num": "07" }, { "num": "08" }, { "num": "09" }, { "num": "10" }, { "num": "11" }, { "num": "12" }];
+  selectedYear: string = (new Date().getFullYear()).toString();
+  selectedMonth: string = (new Date().getMonth() + 1).toString();
   allMonthlyReportsClient: MonthlyReport[] | undefined;
   client: Client;
   clientId: string;
   allMonthlyReports: MonthlyReport[] | undefined;
   myReport: MonthlyReport[] | undefined;
-  y: boolean = false;
   types: string[] = [];
   steps: any[];
-  allFields:stepFieldMonth[];
+  allFields: stepFieldMonth[];
   fieldByType: { [key: string]: stepFieldMonth[] } = {};
-  myDate: Date;;
-  getMonthlyReportsForClientF(date:Date): void {
-    console.log(this.client, 'client');
+
+  getMonthlyReportsForClient(): void {
+    debugger
     const clientId = String(this.client._id);
     this.monthlyReportService.getMonthlyReportForClient(clientId).subscribe(
       (reports) => {
         this.allMonthlyReportsClient = reports;
-        this.myReport = this.allMonthlyReportsClient.filter(m => new Date(m.reportDate).getMonth() + 1 === date.getMonth());
-
+        this.myReport = this.allMonthlyReportsClient.filter(m => new Date(m.reportDate).getMonth() + 1 === Number(this.selectedMonth) && new Date(m.reportDate).getFullYear() === Number(this.selectedYear));
       },
       (error) => {
         console.error('Error fetching monthly reports for client', error);
       }
     );
   }
-  getMonthlyReports(): void {
-    this.monthlyReportService.getAllMonthlyReport().subscribe(
-      (reports) => {
-        this.allMonthlyReports = reports;
-        console.log(this.allMonthlyReports, 'myReport');
-      },
-      (error) => {
-        console.error('Error fetching yearly reports for client', error);
-      }
-    );
-  }
-  myReportf(month: string): void {
-    const currentYear = new Date().getFullYear();
-    const m = Number(month);
-    this.myDate = new Date(currentYear, m, 1)
-    this.getMonthlyReportsForClientF(this.myDate)
-  }
+  // getMonthlyReports(): void {
+  //   this.monthlyReportService.getAllMonthlyReport().subscribe(
+  //     (reports) => {
+  //       this.allMonthlyReports = reports;
+  //     },
+  //     (error) => {
+  //       console.error('Error fetching yearly reports for client', error);
+  //     }
+  //   );
+ // }
+ 
   getStepByType(type: string): void {
     this.steps = this.allMonthlyReports.map((r) =>
       r.monthlyReportFields.filter((r) => r.type === type)
