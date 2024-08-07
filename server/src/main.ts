23b9484274fd89245d7e673dc21258479d94c094
Tzipi-Winefeld <157100435+Tzipi-Winefeld@users.noreply.github.com>
2024-08-08 00:22:04 +0300

    swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('yourTag')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // Enable CORS
   app.enableCors({
-        // origin: process.env.PATH_FRONT,
-    origin: ['http://localhost:4200' ,'https://monoreposmartax-fronted.onrender.com'],// Allow requests from this origin
+    // origin: process.env.PATH_FRONT,
+    origin: [
+      'http://localhost:4200',
+      'https://monoreposmartax-fronted.onrender.com',
+    ], // Allow requests from this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
