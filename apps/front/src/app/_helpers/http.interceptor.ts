456c93006b6a60470f25ea014c8c473c3039835f
Tzipi-Winefeld <157100435+Tzipi-Winefeld@users.noreply.github.com>
2024-08-07 03:58:22 +0300

    inter

--- apps/front/src/app/_helpers/http.interceptor.ts
+++ apps/front/src/app/_helpers/http.interceptor.ts
@@ -1,31 +1,66 @@
+// import { Injectable } from '@angular/core';
+// import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { StorageService } from '../_services/storage.service';
+// import { TokenService } from '../_services/token.service';
+
+
+// @Injectable()
+// export class AuthAndCredentialsInterceptor implements HttpInterceptor {
+
+//   constructor(private tokenService:TokenService){}
+//   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+//     const token = this.tokenService.getToken()?.access_token;
+//     if (token) {
+      
+//       req = req.clone({
+//         setHeaders: {
+//           Authorization: `Bearer ${token}`
+//         }
+//       });
+//     }
+
+//     req = req.clone({
+//       withCredentials: true
+//     });
+
+//     return next.handle(req);
+//   }
+// }
+
+// export const httpInterceptorProviders = [
+//   { provide: HTTP_INTERCEPTORS, useClass: AuthAndCredentialsInterceptor, multi: true },
+// ];
+
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { StorageService } from '../_services/storage.service';
 import { TokenService } from '../_services/token.service';
-
 
 @Injectable()
 export class AuthAndCredentialsInterceptor implements HttpInterceptor {
 
-  constructor(private tokenService:TokenService){}
+  constructor(private tokenService: TokenService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     const token = this.tokenService.getToken()?.access_token;
+    let clonedReq = req;
+
     if (token) {
-      
-      req = req.clone({
+      clonedReq = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
         }
       });
     }
 
-    req = req.clone({
+    clonedReq = clonedReq.clone({
       withCredentials: true
     });
 
-    return next.handle(req);
+    return next.handle(clonedReq);
   }
 }
 
