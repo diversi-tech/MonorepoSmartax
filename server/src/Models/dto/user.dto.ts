d93cc1f6059c610e14ece8e8decab03f5ccb59e6
Tzipi-Winefeld <157100435+Tzipi-Winefeld@users.noreply.github.com>
2024-08-08 15:34:30 +0300

    hashPassword2

--- server/src/Models/dto/user.dto.ts
+++ server/src/Models/dto/user.dto.ts
@@ -82,11 +82,11 @@ export class CreateUserDto {
     @ApiProperty({ description: 'The hashed password' })
     @IsNotEmpty()
     @IsString()
-    @MinLength(8)
-    @MaxLength(20)
-    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
-        message: 'Password must contain at least one uppercase letter, one lowercase letter, and one number',
-    })
+    // @MinLength(8)
+    // @MaxLength(20)
+    // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    //     message: 'Password must contain at least one uppercase letter, one lowercase letter, and one number',
+    // })
     passwordHash: string;
 
     @ApiProperty({ description: 'The role of the user', required: false })
