4c905a838e5fa792bd006de7db96654bb5fbcbc1
Tzipi-Winefeld <157100435+Tzipi-Winefeld@users.noreply.github.com>
2024-08-09 00:14:47 +0300

    fix-merge

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -335,7 +335,7 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:4200','https://monoreposmartax-fronted.onrender.com'],
     // origin: process.env.PATH_FRONT,
-    origin: 'http://localhost:4200',
+    // origin: 'http://localhost:4200',
     // origin: 'https://monoreposmartax-fronted.onrender.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
