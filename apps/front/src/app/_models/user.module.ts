fb2e512d57b0689a9e33127aa4fb5d43819db15f
Tzipi-Winefeld <157100435+Tzipi-Winefeld@users.noreply.github.com>
2024-08-08 18:28:29 +0300

    modelFront

--- apps/front/src/app/_models/user.module.ts
+++ apps/front/src/app/_models/user.module.ts
@@ -9,14 +9,16 @@
 //   role: string;
 //   favoritesClient:string[];
 // }
-import { Client } from "./client.module";
-import { Role } from "./role.module";
+import { Client } from './client.module';
+import { Role } from './role.module';
 
 export interface User {
   _id: string;
   userName: string;
   email: string;
   passwordHash: string;
-  role: Role; // התייחסות לאובייקט Role ולא למחרוזת
-  favoritesClient: Client[]; // התייחסות למערך של אובייקטים מסוג Client
+  role: string;
+  favoritesClient: string[];
+  // role: Role; // התייחסות לאובייקט Role ולא למחרוזת
+  // favoritesClient: Client[]; // התייחסות למערך של אובייקטים מסוג Client
 }
