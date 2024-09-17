62b8b9e415d196fd6431ff4ef4244bb3b28fcddf
EttyLehiman <etti378@gmail.com>
2024-09-17 18:05:27 +0300

    fix Etty

--- server/src/Models/dto/yearlyReport.dbo.ts
+++ server/src/Models/dto/yearlyReport.dbo.ts
@@ -1,126 +1,102 @@
 import { Prop, Schema } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsDateString, IsString } from 'class-validator';
+import {  IsString, IsOptional } from 'class-validator';
 import { StepField } from '../stepField.model';
 import { Status } from '../status.model';
-import { Index } from 'typeorm';
+
 
 @Schema()
 export class CreateYearlyReportDto {
 
-    @Prop()
     @ApiProperty({ example: 'user_id_example' })
-    @IsNotEmpty()
+    @IsOptional()
     idClient: string;
 
-    @Prop()
-    @ApiProperty({ type: [String], example: ['assignee1', 'assignee2'] })
-    @IsNotEmpty()
+    @ApiProperty({  example: ['assignee1', 'assignee2'] })
+    @IsOptional()
     assignee: string[];
 
-    @Prop()
     @ApiProperty({ example: 'employee_id_example' })
-    @IsNotEmpty()
+    @IsOptional()
     idEmploye: string;
 
-    @Prop()
     @ApiProperty({ example: '2023' })
-    @IsNotEmpty()
+    @IsOptional()
     yearReport: string;
 
-    @Prop()
     @ApiProperty({ example: new Date() })
-    @IsNotEmpty()
-    @IsDateString()
+    @IsOptional()
     dateTime: Date;
 
-    @Prop()
     @ApiProperty({ example: 100 })
-    @IsNotEmpty()
+    @IsOptional()
     price: number;
 
-    @Prop()
     @ApiProperty({ example: 50 })
-    @IsNotEmpty()
+    @IsOptional()
     paymentAmountPaid: number;
 
-    @Prop()
     @ApiProperty({ example: 50 })
-    @IsNotEmpty()
+    @IsOptional()
     balanceDue: number;
 
-    @Prop([StepField])
-    @ApiProperty({ type: [StepField] })
-    @IsNotEmpty()
+    @ApiProperty()
+    @IsOptional()
     stepsList: StepField[];
 
-    @Prop()
     @ApiProperty()
-    @IsNotEmpty()
+    @IsOptional()
     @IsString()
     entityType: string;
 
-    @Prop()
     @ApiProperty()
-    @IsNotEmpty()
+    @IsOptional()
     status: Status;
 }
 
 export class UpdateYearlyReportDto {
 
-    @Prop()
     @ApiProperty({ example: 'user_id_example' })
-    @IsNotEmpty()
+    @IsOptional()
     idClient: string;
 
-    @Prop()
-    @ApiProperty({ type: [String], example: ['assignee1', 'assignee2'] })
-    @IsNotEmpty()
+    @ApiProperty({ example: ['assignee1', 'assignee2'] })
+    @IsOptional()
     assignee: string[];
 
-    @Prop()
     @ApiProperty({ example: 'employee_id_example' })
-    @IsNotEmpty()
+    @IsOptional()
     idEmploye: string;
 
-    @Prop()
     @ApiProperty({ example: '2023' })
-    @IsNotEmpty()
+    @IsOptional()
     yearReport: string;
 
-    @Prop()
     @ApiProperty({ example: new Date() })
-    @IsNotEmpty()
-    @IsDateString()
+    @IsOptional()
     dateTime: Date;
 
-    @Prop()
     @ApiProperty({ example: 100 })
-    @IsNotEmpty()
+    @IsOptional()
     price: number;
 
-    @Prop()
     @ApiProperty({ example: 50 })
-    @IsNotEmpty()
+    @IsOptional()
     paymentAmountPaid: number;
 
-    @Prop()
     @ApiProperty({ example: 50 })
-    @IsNotEmpty()
+    @IsOptional()
     balanceDue: number;
 
-    @Prop([StepField])
-    @ApiProperty({ type: [StepField] })
-    @IsNotEmpty()
+    @ApiProperty()
+    @IsOptional()
     stepsList: StepField[];
 
-    @Prop()
     @ApiProperty()
-    @IsNotEmpty()
+    @IsOptional()
     entityType: string;
 
-    @Prop()
     @ApiProperty()
-    @IsNotEmpty()
+    @IsOptional()
     status: Status;
 }
